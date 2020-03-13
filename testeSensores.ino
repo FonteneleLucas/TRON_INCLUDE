161699d3e53202d84406614688bf53cf338d8335
FonteneleLucas <lucasfind@hotmail.com>
2020-03-13 17:09:39 -0300

    biblioteca tron

---
+++ testeSensores.ino
@@ -0,0 +1,35 @@
+#include <Tron.h>
+
+Tron sensor;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (!sensor.inclinacao('C')) {
+    sensor.led_acender('B', "blue");
+    sensor.buzzer('D', 300);
+    sensor.lazer('A');    
+  }
+  else {
+    sensor.led_apagar('B');
+    sensor.buzzer_apagar('D');
+    sensor.lazer_apagar('A');
+  }
+  
+  
+ //sensor.umidade('B');
+  /*
+   if (sensor.inclinacao('C')) {
+    sensor.led_acender('D', "blue");
+    sensor.buzzer('B', 300);
+    sensor.lazer('A');    
+  }
+  else {
+    sensor.led_apagar('D');
+    sensor.buzzer_apagar('B');
+    sensor.lazer_apagar('A');
+  }
+   */
+}
